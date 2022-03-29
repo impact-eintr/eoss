df93e770af05353b1f65e49df36fd26d320ce3d9
impact-eintr <yixingwei4@gmail.com>
2022-03-29 14:20:36 +0800

    修改启动脚本

--- cmd/esq/run.sh
+++ cmd/esq/run.sh
@@ -2,4 +2,4 @@
 
 make
 
-./esqd -http_addr="172.18.0.3:9501" -tcp_addr="172.18.0.3:9502" -raftd_endpoint="172.18.0.4:8001" -node_id=1 -node_weight=1 -data_save_path=./data -enable_cluster=true -enable_raftd=true
+./esqd -http_addr="172.18.0.4:9501" -tcp_addr="172.18.0.4:9502" -raftd_endpoint="172.18.0.3:8001" -node_id=1 -node_weight=1 -data_save_path=./data -enable_cluster=true -enable_raftd=true
